01261e619e7625d3bf1d03e5b9c81af52173cb0f
Ravi Parihar <raviparihar1113@gmail.com>
2025-10-31 13:07:37 +0530

    fixing some file

--- backend/render-build.sh
+++ backend/render-build.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -e
 npm ci
+chmod +x node_modules/.bin/prisma || true
 npx prisma generate
 npm run build
--- backend/render-start.sh
+++ backend/render-start.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 set -e
+chmod +x node_modules/.bin/prisma || true
 npx prisma migrate deploy
 node dist/app.js
